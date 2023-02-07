9347b8afee78a7c8c8724d8f1830e54280fc59ff
Mateus Alves <alvesmateus.ps@gmail.com>
2023-02-07 12:33:32 -0300

    interface

---
+++ Interface/src/br/ufjf/dcc/poo/exemplos/Animal.java
@@ -0,0 +1,20 @@
+package br.ufjf.dcc.poo.exemplos;
+
+public class Animal implements SerVivo {
+
+	@Override
+	public void mover() {
+		
+	}
+
+	@Override
+	public void comer() {
+		
+	}
+
+	@Override
+	public void informação() {
+		
+		
+	}
+}
